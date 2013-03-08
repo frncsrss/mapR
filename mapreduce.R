f7617d1a3abfb63f45639cf0e38e9b5bb39b91e1
François Rousseau <frncs.rss@gmail.com>
2013-03-08 14:44:24 +0100

    hdfs + mapreduce trivial example

---
+++ mapreduce.R
@@ -0,0 +1,14 @@
+# compute the first 1000 squares in memory
+small.ints = 1:1000
+sapply(small.ints, function(x) x^2)
+
+# compute the first 1000 squares with Hadoop (HDFS + MapReduce)
+library(rhdfs)
+library(rmr2)
+small.ints = to.dfs(1:1000)
+from.dfs(
+  mapreduce(
+    input = small.ints, 
+    map = function(k, v) cbind(v, v^2)
+  )
+)
